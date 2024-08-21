dd091030b600a2d1547bf52141872e175d064b8b
Ripunjay choudhury <ripunjaychoudhury42@gmail.com>
2024-08-22 04:06:28 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -106,8 +106,8 @@ const Contact = () => {
         variants={slideIn('right', "tween", 0.2, 1)}
         className="xl:flex-1 xl:h-auto md:h-[550px] h-[350px]"  
       >
-        <EarthCanvas />
-      </motion.div>
+        {/*<EarthCanvas />*/}
+        </motion.div>
     </div>
   )
 }
