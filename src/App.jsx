59686d855a20b4d09c8b01194dd7a7f8f0ab146a
Ripunjay choudhury <ripunjaychoudhury42@gmail.com>
2024-09-25 14:45:02 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <AudioProvider>
       <BrowserRouter>
         <div className="bg-primary relative z-0">
-          <div className="bg-hero-patter bg-cover bg-center bg-no-repeat">
+          <div className="bg-hero-pattern bg-cover bg-center bg-no-repeat">
             <Navbar />
               <div className="relative z-0">
                  <div class="absolute inset-0 z-[-2] bg-black opacity-80"></div> 
