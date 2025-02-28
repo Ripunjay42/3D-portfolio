28d1f8478f560b169a57dc96752ac618eb230893
Ripunjay42 <ripunjaychoudhury42@gmail.com>
2025-02-28 14:49:11 +0530

    Update internship title for CDAC experience

--- src/constants/index.js
+++ src/constants/index.js
@@ -197,7 +197,7 @@ const technologies = [
 
 const experiences = [
   {
-    title: "Project Intern (Full-Stack Development)",
+    title: "Project Intern (Full-Stack Developer)",
     company_name: "CDAC, Bangalore",
     icon: cdac,
     iconBg: "#013247",
